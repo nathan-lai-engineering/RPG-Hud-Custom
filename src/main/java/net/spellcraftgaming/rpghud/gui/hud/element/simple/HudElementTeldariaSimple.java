e74e33a74dd32f55bf6304cd73f0c5f229e74e51
nathan <nlai274@gmail.com>
2024-02-12 20:02:09 -0800

    add vanilla item damage to stat calculation

--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/simple/HudElementTeldariaSimple.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/simple/HudElementTeldariaSimple.java
@@ -1,11 +1,22 @@
 package net.spellcraftgaming.rpghud.gui.hud.element.simple;
 
+import com.google.common.collect.Ordering;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.item.ItemStack;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElement;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementType;
 import net.spellcraftgaming.rpghud.settings.Settings;
 import org.lwjgl.system.SharedLibrary;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collection;
+import java.util.Map;
 
 public class HudElementTeldariaSimple extends HudElement{
     public static final String DEFENSE = "defense";
@@ -25,9 +36,11 @@ public class HudElementTeldariaSimple extends HudElement{
     public static final String CRIT_STAT = "[⚒] Crit";
     public static final String CRIT_CHANCE_TAG = "MMOITEMS_CRITICAL_STRIKE_CHANCE";
     public static final String CRIT_DAMAGE_TAG = "MMOITEMS_CRITICAL_STRIKE_POWER";
-    public static final double BASE_CRITICAL_STRIKE_POWER = 110.0;
+    public static final double BASE_CRITICAL_STRIKE_POWER = 105.0;
 
     protected int tstatOffset = 0;
+    private Attribute a;
+
     public HudElementTeldariaSimple(){
         super(HudElementType.TELDARIA, 0, 0, 0, 0, true);
     }
@@ -89,32 +102,56 @@ public class HudElementTeldariaSimple extends HudElement{
         return switch(statName){
             case DEFENSE -> String.format("%s: %.1f", DEFENSE_STAT, getStat(DEFENSE_TAG));
             case BLOCK -> String.format("%s: %.1f%% | %.1f%%", BLOCK_STAT, getStat(BLOCK_RATE_TAG), getStat(BLOCK_POWER_TAG));
-            case DAMAGE -> String.format("%s: %.2f", DAMAGE_STAT, getStat(DAMAGE_TAG));
-            case CRIT -> String.format("%s: %.1f%% | %.1f%%", CRIT_STAT, getStat(CRIT_CHANCE_TAG), getStat(CRIT_DAMAGE_TAG));
+            case DAMAGE -> String.format("%s: %.1f", DAMAGE_STAT, getStat(DAMAGE_TAG));
+            case CRIT -> String.format("%s: %.1f%% | %.1f%%", CRIT_STAT, getStat(CRIT_CHANCE_TAG), getStat(CRIT_DAMAGE_TAG) + BASE_CRITICAL_STRIKE_POWER);
             default -> "";
         };
     }
 
     /**
      * Summation of a particular stat from all currently equipped stuffs
-     * @param statName
+     * @param statTag
      * @return
      */
-    private Double getStat(String statName){
+    private Double getStat(String statTag){
         double stat = 0;
+        if(statTag.equals(DAMAGE_TAG)) {
+            stat = 1;
+            Collection<MobEffectInstance> collection = this.mc.player.getActiveEffects();
+            if(!collection.isEmpty()) {
+                int strengthBoost = 0;
+                for(MobEffectInstance effectinstance : Ordering.natural().reverse().sortedCopy(collection)) {
+                    if(effectinstance.getEffect().getDisplayName().getString().equals("Strength")){
+                        strengthBoost = Math.max(strengthBoost, (1 + effectinstance.getAmplifier()) * 3);
+                    }
+                }
+                stat += strengthBoost;
+            }
+        }
         for (int i = this.mc.player.getInventory().armor.size() - 1; i >= 0; i--) {
             if (this.mc.player.getInventory().getArmor(i) != ItemStack.EMPTY
                     && this.mc.player.getInventory().getArmor(i).getItem().isDamageable(null)) {
                 ItemStack item = this.mc.player.getInventory().getArmor(i);
-                if(item.getShareTag() != null && item.getShareTag().contains(statName)){
-                    stat += Double.parseDouble(item.getTag().get(statName).getAsString());
+                if(item.getShareTag() != null && item.getShareTag().contains(statTag)){
+                    stat += Double.parseDouble(item.getTag().get(statTag).getAsString());
                 }
             }
         }
         if (this.mc.player.getMainHandItem() != ItemStack.EMPTY) {
             ItemStack item = this.mc.player.getMainHandItem();
-            if(item.getShareTag() != null && item.getShareTag().contains(statName)){
-                stat += Double.parseDouble(item.getTag().get(statName).getAsString());
+            if(item.getShareTag() != null && item.getShareTag().contains(statTag)){
+                stat += Double.parseDouble(item.getTag().get(statTag).getAsString());
+            }
+            else if(statTag.equals(DAMAGE_TAG)){
+                double itemAttack = 0;
+                for(net.minecraft.world.entity.ai.attributes.Attribute a: item.getAttributeModifiers(EquipmentSlot.MAINHAND).keys()){
+                    if(a.getDescriptionId().equals("attribute.name.generic.attack_damage")){
+                        for(net.minecraft.world.entity.ai.attributes.AttributeModifier am: item.getAttributeModifiers(EquipmentSlot.MAINHAND).get(a)){
+                            itemAttack += am.getAmount();
+                        }
+                    }
+                }
+                stat += itemAttack;
             }
         }
         return stat;
